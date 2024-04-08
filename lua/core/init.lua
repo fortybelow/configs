77f0c165fa0ca0b153cc42c5e85e956d8c75ed60
batman <batman@proxy.com>
2024-04-08 09:30:28 -0400

    local changes

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -17,10 +17,10 @@ opt.cursorline = true
 
 -- Indenting
 opt.expandtab = true
-opt.shiftwidth = 2
+opt.shiftwidth = 4
 opt.smartindent = true
-opt.tabstop = 2
-opt.softtabstop = 2
+opt.tabstop = 4
+opt.softtabstop = 4
 
 opt.fillchars = { eob = " " }
 opt.ignorecase = true
@@ -49,7 +49,7 @@ opt.updatetime = 250
 -- when cursor reaches end/beginning of line
 opt.whichwrap:append "<>[]hl"
 
-g.mapleader = " "
+g.mapleader = ","
 
 -- disable some default providers
 for _, provider in ipairs { "node", "perl", "python3", "ruby" } do
